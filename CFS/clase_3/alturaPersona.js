91513707ff128e85c7ccf5dc6cecd846fb79d53e
Tomas Galotti <root@VD-Code.garaje>
2024-04-29 23:43:24 +0000

    clase 3

--- CFS/clase_3/alturaPersona.js
+++ CFS/clase_3/alturaPersona.js
@@ -1,5 +1,13 @@
 "use strict";
+/*
+import * as readlineSync from "readline-sync";
+const alturaPersona = readlineSync.question("ingrese la altura:");
+console.log("la altura de la persona es: " + alturaPersona);
+*/
 Object.defineProperty(exports, "__esModule", { value: true });
 var readlineSync = require("readline-sync");
-var alturaPersona = readlineSync.question("ingrese la altura:");
-console.log("la altura de la persona es: " + alturaPersona);
+var precio = readlineSync.question("ingrese el precio del producto:");
+var descuento = 10; //descuento en %
+var precioFinal;
+precioFinal = (precio - (precio * descuento / 100));
+console.log("El precio a pagar es de: $" + precioFinal);
--- CFS/clase_3/alturaPersona.ts
+++ CFS/clase_3/alturaPersona.ts
@@ -1,3 +1,13 @@
+/*
 import * as readlineSync from "readline-sync";
 const alturaPersona = readlineSync.question("ingrese la altura:");
 console.log("la altura de la persona es: " + alturaPersona);
+*/
+
+import * as readlineSync from "readline-sync";
+let precio : number = readlineSync.question("ingrese el precio del producto:");
+let descuento : number = 10; //descuento en %
+let precioFinal : number;
+precioFinal = (precio - (precio * descuento / 100));
+console.log("El precio a pagar es de: $" + precioFinal);
+
