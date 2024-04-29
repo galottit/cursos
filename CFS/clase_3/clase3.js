ef3bee15501ac922c027840142d34a0c9c5a4283
Tomas Galotti <root@VD-Code.garaje>
2024-04-29 23:40:37 +0000

    clase 3

---
+++ CFS/clase_3/clase3.js
@@ -0,0 +1,6 @@
+var texto1 = "Una frase con \'comillas simples\' dentro";
+var texto2;
+var texto3 = "una frase con \n Una nueva linea dentro";
+var texto4 = "Una frase con \\ Una barra inclinada dentro";
+var texto5 = "Una frase con \t un tabulador dentro";
+console.log(texto4 + "aca empiesa el texto 4+4: " + texto4 + texto5);
---
+++ CFS/clase_3/clase3.ts
@@ -0,0 +1,10 @@
+/*
+let texto1 : string = "Una frase con \'comillas simples\' dentro";
+let texto2 : string;
+let texto3 : string = "una frase con \n Una nueva linea dentro";
+let texto4 : string = "Una frase con \\ Una barra inclinada dentro";
+let texto5 : string = "Una frase con \t un tabulador dentro";
+
+console.log(texto4 + "aca empiesa el texto 4+4: " + texto4 + texto5);
+*/
+
